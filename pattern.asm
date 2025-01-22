4e1d9f40d0c1a7d2f2de76dd7309e53bcf374013
Leanne Biju <148006830+leannebiju@users.noreply.github.com>
2025-01-22 23:05:27 +0530

    Update pattern.asm

--- pattern.asm
+++ pattern.asm
@@ -12,7 +12,8 @@ data ENDS
 
 code segment
     assume ds:data,cs:code
-    start:
+    
+	start:
         mov ax,data
         mov ds,ax
 
@@ -30,38 +31,39 @@ code segment
 
         mov bl,al      
         
-		mov ch, al		;storing to ch also since al changes 
-		inc ch
+	mov ch, al	;storing to ch also since al changes 
+	inc ch
 		
-		mov cl,01H      ;first number
+	mov cl,01H      ;first number
 		
-		loop1:
-        mov dl,' '
+	loop1:
+        
+	mov dl,' '
         mov ah,02H
         int 21h
 
         dec bl
         JNZ loop1
 		
-		;we need to print cl times
-		mov bh, cl
+	;we need to print cl times
+	mov bh, cl
 		
-		;adding loop and logic to print n times
-		loop2:
-        add cl,30H
+	;adding loop and logic to print n times
+	
+	loop2:
+        
+	add cl,30H
         mov dl,cl
         sub cl,30H
         mov ah,02H
         int 21H
 		
+	mov dl, ' '
+	mov ah, 02h
+	int 21h
+	dec bh
+	jnz loop2
 		
-		mov dl, ' '
-		mov ah, 02h
-		int 21h
-		dec bh
-		jnz loop2
-		
-
         lea dx,newline
         mov ah,09H
         int 21H
@@ -70,14 +72,13 @@ code segment
         cmp cl,ch
         jz exit
 		
-		;added 3 line logic to set the spaces 
-		mov al, ch
-		sub al,cl
-		mov bl,al
+	;added 3 line logic to set the spaces 
+	mov al, ch
+	sub al,cl
+	mov bl,al
+	jmp loop1
 		
-        jmp loop1
-		
-		exit:
+	exit:
         mov ah,4CH
         int 21H
 
